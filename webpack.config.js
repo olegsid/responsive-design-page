9afffb54277213f397080994467306fe877519d9
Oleg Shevchuk <olegsid93@gmail.com>
2019-07-02 23:38:03 +0300

    add(reset.css)

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
         ]
       },
       {
-        test: /\.scss$/,
+        test: /\.(css|scss|sass)$/,
         use: [
           'style-loader',
           MiniCssExtractPlugin.loader,
